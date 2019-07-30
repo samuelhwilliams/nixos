a70d509c1623c4bca3546d0c97791a0676b730e8
Samuel Williams <samuelhwilliams@gmail.com>
2019-07-30 12:35:51 +0100

    Initial setup for Companion homeserver

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# nixos
+# NixOS
 Nix configuration files and assorted accoutrements
+
+## Setting up a machine
+Follow instructions [here](https://nixos.org/nixos/manual/index.html#sec-installation).
+
+When it comes to editing `configuration.nix` and/or `hardware-configuration.nix`:
+
+* Clone this repository into `/etc/nixos` on the target disk.
+* Create symlinks the `machines/<hostname>/{configuration,hardware-configuration}.nix` from this repository into `/etc/nixos` on the target disk, overwriting *if appropriate*. Consider hardware/filesystem changes carefully.
+
+Finish the install with `nixos-install`.
---
+++ machines/companion/configuration.nix
@@ -0,0 +1,65 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Allow unfree packages to be installed (e.g. plex)
+  nixpkgs.config.allowUnfree = true;
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sdc";
+
+  # Define hostname
+  networking.hostName = "companion";
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "uk";
+    defaultLocale = "en_GB.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+  ];
+
+  # List services that you want to enable:
+  services.openssh.enable = true;
+
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.sam = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    openssh.authorizedKeys.keyFiles = [ ../../keys/yubikey.pub ];
+  };
+
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
+}
